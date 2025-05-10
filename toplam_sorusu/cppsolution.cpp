b6b2eef68779fcf4e2b39a0ac2f7626466bdddd3
aloima <acar.aloima@gmail.com>
2025-05-10 21:53:00 +0300

    documentize c++ solution of toplam_sorusu

--- toplam_sorusu/cppsolution.cpp
+++ toplam_sorusu/cppsolution.cpp
@@ -1,31 +1,49 @@
 #include <iostream>
-//15  =  7+8; 1+2+3+4+5    yani 15=2
+
 using namespace std;
-// test case denemek laızm
-int main(){
 
-int count=0;
-int num;
-cin >> num;
+/**
+ * @brief Get the count of consecutive numbers group count
+ * 
+ * @param value The number will be sum of a consuctive number group.
+ * @return int
+ */
+int get_count_of_groups(int value) {
+    if (value == 1) return 0;
 
-int i = 2;
-if(num == 1){
-    cout << "0" << endl;
-    return 0;
+    int count = 0;
+    int i = 2;
+
+    /**
+     * @brief Iterates subtracting the sum of 1..i from value and checking if it is divisible by i.
+     * 
+     * 1) 1..i is writable as sum of `i` numbers.
+     * 2) "(value - (1..i)) is writable as `i` numbers" statement should be approved.
+     * If a number is divisible by `i`, it can be written as `i` numbers.
+     * 
+     * For example, if value is 15:
+     * i = 2 | (15 - (1 + 2)) % 2 = 0, count += 1
+     * i = 3 | (15 - (3 + 3)) % 3 = 0, count += 1
+     * i = 4 | (15 - (6 + 4)) % 4 = 1, continue
+     * i = 5 | (15 - (10 + 5)) % 5 = 0, count += 1
+     * i = 6, it breaks.
+     */
+    while (value >= (i * (i + 1) / 2)) {
+        if((value - (i * (i + 1) / 2)) % i == 0) { 
+            count++;
+        }
+
+        i++;
+    }
+
+    return count;
 }
 
-while (num >= i*(i+1)/2)
-{
-if((num-i*(i+1)/2)%i == 0){   // num == 45 i= 2      123456789    22 23     5678910    141516     7891011 
-    count++;
-                    
-}
-i++;
-}
+int main() {
+    int value;
+    cin >> value;
 
-// i = 3 num = 1000000000  
-
-
-cout << count << endl;
+    const int count = get_count_of_groups(value);
+    cout << count << endl;
     return 0;
 }
