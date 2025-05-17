de6c039d44eb799a9dceab8dade453de6f424d24
aloima <acar.aloima@gmail.com>
2025-05-17 21:18:20 +0300

    documentize c++ code of text_editor

--- Text_editor/cppcode.cpp
+++ Text_editor/cppcode.cpp
@@ -1,10 +1,20 @@
-#include <bits/stdc++.h>    
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
 using namespace std;
-int hata = 0;
-//1- iki sesli yan yana gelemez
-//2- bir cümle max 4 kelime olabilir
-//3- bir kelime max 7 harf olabilir
-//4- toplam 30 kelime geçilmemeli
+
+int errors = 0;
+
+/**
+ * @brief Splits string using the delimiter, for example;
+ * "i love you" and " " is {"i", "love", "you"}
+ * 
+ * @param str 
+ * @param delimiters 
+ * @return vector<string>
+ */
 vector<string> tokenize(const string &str, const string &delimiters) {
     vector<string> tokens;
     size_t start = 0, end;
@@ -13,8 +23,10 @@ vector<string> tokenize(const string &str, const string &delimiters) {
         if (end != start) {
             tokens.push_back(str.substr(start, end - start));
         }
-        start = end + 1;
+
+        start = (end + 1);
     }
+
     if (start < str.size()) {
         tokens.push_back(str.substr(start));
     }
@@ -22,57 +34,77 @@ vector<string> tokenize(const string &str, const string &delimiters) {
     return tokens;
 }
 
-void vowelfunc(string str){
-  
-   vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
-    for (int i = 0; i < str.length(); i++)
-    {
-      int j=i+1;
+/**
+ * @brief Counts vowel groups includes more than one vowel in the string as errors.
+ * For example, add 2 to errors variable for "ateasdoea"
+ * 
+ * @param str 
+ */
+void vowelfunc(string str) {
+    vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+
+    for (int i = 0; i < str.length(); i++) {
+      int j = (i + 1);
       auto it1 = find(vowels.begin(), vowels.end(), str[i]);
       auto it2 = find(vowels.begin(), vowels.end(), str[j]);
+
       if(it1 != vowels.end() && it2 != vowels.end()) {
-        ++hata;
+        ++errors;
       }
+    }    
+}
+
+/**
+ * @brief Counts words includes more than 7 characters as errors and add 1 to totalwordcnt for each word.
+ * 
+ * @param words 
+ * @param totalwordcnt 
+ */
+void check_words(vector<string> words, int *totalwordcnt) {
+    for (string s : words) {
+        if (s.length() > 7){
+            errors++;
+        }
+
+        (*totalwordcnt)++;
     }
-    
+}
+
+/**
+ * @brief Counts all words in input and if word count is more than 30, it counts a error.
+ * Also, counts all words in each sentence and if word count in a sentence is more than 4, it counts a error.
+ * Finally, checks all words in a sentence using check_words() method.
+ * 
+ * @param input 
+ */
+void check_sentences(string input) {
+    int totalwordcnt = 0;
+
+    vector<string> sentences = tokenize(input, ".");
+    vector<string> words; // temporary words vector for each sentence, resets on a new sentence
+
+    for (int i = 0; i < sentences.size(); i++) {
+        words = tokenize(sentences[i], " ");
+        check_words(words, &totalwordcnt);
+
+        if(words.size() > 4) {
+            errors++;
+        }
+    }
+
+    if (totalwordcnt > 30){
+        errors++;
+    }
 }
 
 
+int main() {
+    string input;
+    cin >> input; /**< input format should be "it is in asked questions repository of algolab. this question name is text editor." */
 
-int main(){
-    int wordcnt =0;
-    int totalwordcnt=0;
-    string test = "baris kaan nilay algolab. seçim ucube kaan reis.";
-    vowelfunc(test); 
-    vector<string>vec2; // kelimelerin tutulacağı vector
-    vector<string> vec = tokenize(test, "."); // cümlelerin tutulacağı vector
-     for(string s: vec){
-        
-        //cout<< s <<endl; // cümleler yazdırılır
+    vowelfunc(input);
+    check_sentences(input);
 
-    }
-   // cout << " ---------------------------------------" << endl;
-    for (int i = 0; i < vec.size(); i++)
-    {
-      wordcnt=0;
-    vec2 = tokenize(vec[i]," ");  // cümlenin içindeki her bir kelime vec2ye atılır
-      for(string s: vec2){
-        //cout<< s <<endl;
-        wordcnt++;
-        if(s.length()>7){
-            hata++;
-        }
-        totalwordcnt++;
-    }
-    if(wordcnt>4){
-        hata++;
-    }
-    }
-     if(totalwordcnt>30){
-        hata++;
-     }
-     cout << hata;
-
-
+    cout << errors;
     return 0;
 }
