ea217df1b0424cfe99aa790ce4fa4ff4d152d1df
aloima <acar.aloima@gmail.com>
2025-05-17 23:19:16 +0300

    documentize lexicographical_order and theorem_yoda

--- lexicographical_order/lexico_order.cpp
+++ lexicographical_order/lexico_order.cpp
@@ -1,31 +1,47 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+
 using namespace std;
-//lexicographical order
+
+/**
+ * @brief Writes numbers by string lexicographical order. For example;
+ * lexicalOrder(12)
+ * "1 10 11 12 2 3 4 5 6 7 8 9"
+ * 
+ * @param n 
+ */
 void lexicalOrder(int n) {
-    vector<int>normal;
-        for(int i=0; i<n; ++i){
-            normal.push_back(i+1);
-        }
-    vector<string>lexical;
-        for(int i=0; i<n; ++i){
-            string str= to_string(normal[i]);
-            lexical.push_back(str);
-        }
+    vector<int> normal; // stores 0 to n numbers as int
 
-    sort(lexical.begin(), lexical.end());
-
-      for(int i=0; i<n; ++i){
-        cout<< lexical[i]<< " ";
-    }   
+    for(int i=0; i < n; ++i){
+        normal.push_back(i+1);
     }
 
-int main()
-{
+    vector<string> lexical;
+
+    for(int i=0; i < n; ++i){
+        string str = to_string(normal[i]);
+        lexical.push_back(str);
+    }
+
+    // sort() makes lexicographically orders for strings.
+    sort(lexical.begin(), lexical.end());
+
+    for(int i = 0; i < n; ++i) {
+        cout<< lexical[i]<< " ";
+    }
+}
+
+int main() {
     int q;
-    cin>>q;
+    cin >> q;
+
+    // Makes lexical orders for Q numbers.
     while(q--){
         int n;
-        cin>>n;
+        cin >> n;
         lexicalOrder(n);
     }
     
--- theorem_yodas/cppsolution.cpp
+++ theorem_yodas/cppsolution.cpp
@@ -1,39 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <array>
+#include <chrono>
 
-#include <bits/stdc++.h>
-// İÇ İÇE 4 FOR KULLAN KÖKn E KADAR GİDECEĞİ İÇİN O KÖKN^4 OLUYO = ON'2
-// her sayı 4 ayrı doğal sayının karelerinin toplamı olarak yazılabilir
-// BU SORUYU HARCAMA    0 100   soruyu ve outputu düzgün açıklamak gerekiyo    en büyük olanı  boşluk olmicak vs
 using namespace std;
 using namespace std::chrono;
-/*void func(int number) {  
-    int temp = number;
-    vector<int> final(4,0);
-    for(int i = 0; i < 4; ++i) {
-        int sqrt_val = sqrt(temp);  
-        final[i] = sqrt_val; 
-        temp = temp - pow(sqrt_val, 2);
-    }
 
-   if(temp == 0){
-        cout << final[0] << final[1] << final[2] << final[3]<<endl;
-    }
-    else{
-              // 6 4 2 0
-        final[0]--;      
-        temp = number - pow( final[0], 2) ;
-        for(int i = 1; i < 4; ++i) {
-       int sqrt_val2 = sqrt(temp);
-        final[i]= sqrt_val2;
-        temp -= pow( sqrt_val2, 2);   , 
-        
-    }
-     cout << final[0] << final[1] << final[2] << final[3] << endl;
-    }
-}*/
-
+/**
+ * @brief Finds a equality for num = a^2 + b^2 + c^2 + d^2. If cannot find, result will be a = b = c = d = 0
+ * Its time complexity is O(N^2) because for loops starts from sqrt(num), not sqrt(num).
+ * 
+ * @param num
+ * @return array<int, 4> [a, b, c, d]
+ */
 array<int, 4> func(int num) {
     array<int, 4> ans = {0, 0, 0, 0};
     int sqr = sqrt(num);
+
     for (int i = sqr; i >= 0; --i) {
         for (int j = sqr; j >= 0; --j) {
             for (int k = sqr; k >= 0; --k) {
@@ -46,31 +29,17 @@ array<int, 4> func(int num) {
             }
         }
     }
-    return ans; // In case no combination is found
+
+    return ans;
 }
 
-
-/*int main() {
-     std::chrono::time_point<std::chrono::system_clock> start, end;
- 
-    start = std::chrono::system_clock::now();
-    int number=195126;
-    
-
-    array<int, 4> result = func(number);
-
-    end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end-start;
-    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
-
-    cout << result[0] << " " << result[1] << " " << result[2] << " " << result[3] << endl;
-    return 0;
-}*/
-
 int main() {
-    auto start = high_resolution_clock::now(); // Hassas zaman ölçümü
-    
-    int number = 96;
+    // It gives sensitive time. We use this to calculate elapsed seconds.
+    auto start = high_resolution_clock::now();
+
+    int number;
+    cin >> number;
+
     array<int, 4> result = func(number);
 
     auto end = high_resolution_clock::now();
