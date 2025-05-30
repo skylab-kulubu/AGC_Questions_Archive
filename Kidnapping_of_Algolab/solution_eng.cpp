677d0509058e0415126a1b23bcba826b35f5d026
aloima <acar.aloima@gmail.com>
2025-05-30 19:00:09 +0300

    replace c++ solution of kidnapping of algolab

--- Kidnapping_of_Algolab/solution_eng.cpp
+++ Kidnapping_of_Algolab/solution_eng.cpp
@@ -1,54 +1,62 @@
 #include <iostream>
-#include <unordered_set>
+#include <iterator>
+#include <ostream>
+#include <set>
 /**
-    * @brief AGC Sorusu 4
+    * @brief AGC Question 4
     * This function simulates a game where people are eliminated based on bullets.
     * The bullets are represented as an array, where 0 means the bullet is not used and 1 means it is used.
     * The function iterates through the people and bullets, eliminating people based on the bullets used.
     * The last person remaining is printed.
     * 
-    * @param bulletcount: number of bullets
+    * @param bullet_count: number of bullets
     * @param bullets: array of bullets, 0 means not used, 1 means used
-    * @param Peoplecount: number of people
-    * @param [out] people: set of people, each person is represented by an integer called by reference so that it can be modified directly
+    * @param people_count: number of people
+    * @param [out] people: set of people, each person is represented by an integer from 1 to N called by reference so that it can be modified directly
  */
-void elimination(int bullet_count, int bullets[],long int people_count,std::unordered_set<int> &people){
+void elimination(int bullet_count, int bullets[], long int people_count, std::set<int> &people){
     int i = 0, j=0; 
     auto it = people.begin();
-    for(int k=0; k<people_count; k++){
-        people.insert(k+1); 
-    }
-    while(people.size()>1){
-        
-        j %= people.size();
-        
-        std::advance(it,j);   
 
-        i = i % bullet_count; 
-        
-        if(bullets[i]==0){
-            bullets[i]=1;
-        } else {
-            bullets[i]=0;
-            people.erase(it);
-            j--;
+    // final: O(N)
+    for(int k = 0; k < people_count; k++) { // O(N)
+        people.insert(it, k + 1); // insert(pos, value) is amortized constant, ~O(1)
+        std::advance(it, 1); // advance(it, n) is O(n) but n = 1, so it is O(1)
+    }
+
+    it = people.begin();
+
+    // final: O(N)
+    while (people.size() > 1) { // O(N)
+        if (it == people.end()) {
+            it = people.begin();
         }
-        
-        it = people.begin();
-        
+
+        i = i % bullet_count;
+
+        if(bullets[i] == 0) {
+            bullets[i] = 1;
+            std::advance(it, 1); // O(N) but N = 1, O(1)
+        } else {
+            bullets[i] = 0;
+
+            auto next_iterator = std::next(it); // std::advance(it, 1) but returns new it, also O(1)
+            people.erase(it); // amortized constant, ~O(1)
+            it = next_iterator;
+        }
+
         i++;
-        j++;
     }
 }
+
 int main() {
-   
-    std::unordered_set<int> people;
+    std::set<int> people;
 
     int bullet_count;
     std::cin >> bullet_count; 
 
     int bullets[bullet_count];
-    for(int i=0; i<bullet_count; i++){
+    for(int i = 0; i < bullet_count; i++){
         std::cin >> bullets[i];
     }
 
@@ -56,6 +64,5 @@ int main() {
     std::cin >> people_count;
  
     elimination(bullet_count, bullets, people_count, people);
-    
     std::cout << *(people.begin()) << std::endl;
 }
