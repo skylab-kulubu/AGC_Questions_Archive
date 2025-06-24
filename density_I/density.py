7f9485ba9523172d082ed238def37bafdc91f1f2
Stealthhy7512 <kaanyazici7512@gmail.com>
2025-06-24 23:14:14 +0300

    Add `density_I` Python function documentation.

--- density_I/density.py
+++ density_I/density.py
@@ -1,14 +1,16 @@
-# Density 1 python solution.
-
-import random
-import itertools
-
-
 def density_i(lst: list[int]) -> int:
-    current_count = 0
-    subarray_sum = 0
-    max_count = 0
-    max_sum = 0
+    """Finds and returns the sum of the longest adjacent grouping in a list. Group with the larger
+    sum is selected if there are more than one groups with the same length.
+
+    >>> density_i([1, 1, 0, 2, 2])
+    4
+
+    :param lst: List of numbers to be searched.
+
+    :return: Sum of the largest group.
+    """
+    current_count, subarray_sum, max_count, max_sum = 0
+
     for num in lst:
         if num != 0:
             current_count += 1
