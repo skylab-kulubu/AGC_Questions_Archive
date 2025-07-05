2187cb1366aecc236b0a5597a3f4ff5de6e356a6
Stealthhy7512 <kaanyazici7512@gmail.com>
2025-07-05 20:09:22 +0300

    Document `bridge_deque.py` and add README for `bridge_riddle`

---
+++ bridge_riddle/README.md
@@ -0,0 +1,20 @@
+In the village of Algolab 🏡, a group of villagers $P$ start on the left side ⬅️ of a cursed bridge, needing to cross to the right ➡️ before darkness consumes them 🌑. They have only one magical torch 🔥✨, and the bridge allows at most two people at a time 🚶‍♂️🚶‍♀️. Each villager moves at their own fastest pace, but they can slow down at will, and when two cross together, they must move together as the magical torch illuminates little ⏳🐢. After each crossing, someone must return with the torch for the others.
+
+Given any $n$ villagers with respective speeds $P_i$, devise an algorithm to determine the minimum total time $t$ required for all to cross safely ✅.
+
+Can you help Algolab escape the night’s curse? 🌙🕰️
+
+---
+
+**Input Format**
+> First line is an integer $n$, the number of people,
+>
+> Following $n$ integers represent $P_i$, the maximum pace of each person.
+
+**Output Format**
+> An integer $t$, minimum required time for all to pass safely.
+
+**Constraints**
+> $ 1 \le n \le 10^6$
+>
+> $1 \le P \le 10^9 $
--- bridge_riddle/bridge_deque.py
+++ bridge_riddle/bridge_deque.py
@@ -1,38 +1,53 @@
 from collections import deque
 
-# O(n*lgn + lgn) => O(n*lgn)
 def bridge_riddle(people_l: list[int]) -> int:
-  # O(n*lgn)
+  """Returns how much time is needed to have people cross to right side of the bridge.
+
+  This involves a strategy that repeats the following steps until 3 persons are left on the left side:
+  1) Send two fastest people.
+  2) Have the faster of the pair return the torch.
+  3) Send two slowest people.
+  4) Have the remaining fast person return the torch.
+  5) Repeat steps 1 through 4 until 3 people or less remain on the left.
+
+  In each step, total time required is incremented based on the slowest person crossing.
+  After having 3 or less people remained, their total pace is added to total time to finish.
+
+  Summing 3 person's pace is a shortcut, instead, the loop might also be continued.
+
+  >>> bridge_riddle([1, 4, 16])
+  21
+
+  >>> bridge_riddle([2, 1, 5, 10])
+  17
+
+  :param people_l: List of pace of every individual person that are on the left side on the bridge.
+
+  :return: Total time needed for all to cross in the quickest way possible.
+  """
   people_l = deque(sorted(people_l))
   people_r = deque()
 
   time = 0
 
-  # O(lgn) iteration
   while len(people_l) > 3:
-    # Constant pop
     fast_1, fast_2 = people_l.popleft(), people_l.popleft()
     time += max(fast_1, fast_2)
 
-    # Constant insert
     people_r.appendleft(fast_2)
     people_r.appendleft(fast_1)
 
     time += (fast_r := people_r.popleft())
 
-    # Constant insert
     people_l.appendleft(fast_r)
 
-    # Constant pop
     slow_1, slow_2 = people_l.pop(), people_l.pop()
     time += max(slow_1, slow_2)
 
-    # Constant insert
     people_r.append(slow_2)
     people_r.append(slow_1)
 
     time += (fast := people_r.popleft())
-    # Constant insert
     people_l.appendleft(fast)
 
   time += (sum(person for person in people_l) if len(people_l) == 3 else max(people_l))
@@ -40,15 +55,8 @@ def bridge_riddle(people_l: list[int]) -> int:
   return time
 
 
-def main() -> None:
-    nums = [1, 4, 2, 5, 3]
-    nums1 = [1, 2, 5, 10]
-    nums2 = [1, 1, 1, 1, 1, 2]
-    assert bridge_riddle(nums) == 16
-    assert bridge_riddle(nums1) == 17
-    assert bridge_riddle(nums2) == 10
-
-
 if __name__ == '__main__':
-    main()
+    input()
+    people = list(map(int, input().split()))
+    print(bridge_riddle(people))
 
